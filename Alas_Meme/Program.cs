2a6adbd247e6bea90be85f2f87255cc2ddf8c96c
SaigyoujiYukon <misaka09983@gmail.com>
2023-01-15 04:12:55 +0800

    Finish Program 反正现在就是能自动把md转换成图片啦

--- Alas_Meme/Program.cs
+++ Alas_Meme/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using MarkdownDeep;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Alas_Meme
 {
@@ -13,17 +14,37 @@ namespace Alas_Meme
         public static void Main(string[] args)
         {
             //Read the markdown file
-            // string markdown = File.ReadAllText(args[0]);
-            string markdown = File.ReadAllText("../../../Alas_Meme_N.md");
+            string markdown = null;
+            try
+            {
+                markdown = File.ReadAllText(args[0]);
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                markdown = File.ReadAllText("../../../Alas_Meme_N_Debug.md");
+                // markdown = File.ReadAllText("../../../Alas_Meme_N.md");
+            }
 
             //Initialize the MarkdownDeep engine
             Markdown md = new Markdown();
-
+            
             //Convert the markdown to HTML
             string html = md.Transform(markdown);
 
+            //Process image width
+            html = Regex.Replace(html, @"(<img.*?)(/?>)", "$1 width=\"400\"$2");
+
             //Save the HTML to a file
-            File.WriteAllText("../../../temp/output.html", html);
+            try
+            {
+                File.WriteAllText("../../../temp/output.html", html);
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory("../../../temp");
+                File.WriteAllText("../../../temp/output.html", html);
+            }
+            
             Console.WriteLine("Markdown converted to HTML successfully!");
 
             //Convert the HTML to image
@@ -32,8 +53,9 @@ namespace Alas_Meme
             browser.ScrollBarsEnabled = false;
             browser.AllowNavigation = false;
             browser.DocumentText = html;
-            browser.Width = 800;
-            browser.Height = 2000;
+            // browser.Width = 800;
+            // browser.Height = 2000;
+            
             browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser_DocumentCompleted);
             Application.Run();
         }
@@ -41,11 +63,27 @@ namespace Alas_Meme
         static void WebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
             var browser = sender as WebBrowser;
+            
+            //scale browser to 200%
+            browser.Document.Body.Style = "zoom: 300%;";
+            browser.Width = browser.Document.Body.ScrollRectangle.Width;
+            browser.Width = browser.Width * 3;
+            browser.Height = browser.Document.Body.ScrollRectangle.Height;
+            browser.Height = browser.Height * 3;
+            if (browser.Width < 920)
+            {
+                browser.Width = 920;
+            }
+            
             using (Bitmap bitmap = new Bitmap(browser.Width, browser.Height))
             {
                 browser.DrawToBitmap(bitmap, new Rectangle(0, 0, browser.Width, browser.Height));
                 bitmap.Save("../../../temp/output.png", ImageFormat.Png);
                 Console.WriteLine("HTML convert to image successfully!");
+                
+                //del output.html
+                File.Delete("../../../temp/output.html");
+                
                 Application.Exit();
             }
         }
