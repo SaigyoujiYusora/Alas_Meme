836ab75f9a4d58c6020869c0e8d3526cf498c8d9
SaigyoujiYukon <misaka09983@gmail.com>
2023-01-15 01:37:12 +0800

    finish Program.cs

--- Alas_Meme/Program.cs
+++ Alas_Meme/Program.cs
@@ -9,10 +9,12 @@ namespace Alas_Meme
 {
     internal class Program
     {
+        [STAThread]
         public static void Main(string[] args)
         {
             //Read the markdown file
-            string markdown = File.ReadAllText(args[0]);
+            // string markdown = File.ReadAllText(args[0]);
+            string markdown = File.ReadAllText("../../../Alas_Meme_N.md");
 
             //Initialize the MarkdownDeep engine
             Markdown md = new Markdown();
@@ -21,16 +23,17 @@ namespace Alas_Meme
             string html = md.Transform(markdown);
 
             //Save the HTML to a file
-            File.WriteAllText("output.html", html);
+            File.WriteAllText("../../../temp/output.html", html);
             Console.WriteLine("Markdown converted to HTML successfully!");
 
             //Convert the HTML to image
             var browser = new WebBrowser();
+            Console.WriteLine("Web Open Successfully!");
             browser.ScrollBarsEnabled = false;
             browser.AllowNavigation = false;
             browser.DocumentText = html;
-            browser.Width = 1024;
-            browser.Height = 800;
+            browser.Width = 800;
+            browser.Height = 2000;
             browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser_DocumentCompleted);
             Application.Run();
         }
@@ -41,7 +44,7 @@ namespace Alas_Meme
             using (Bitmap bitmap = new Bitmap(browser.Width, browser.Height))
             {
                 browser.DrawToBitmap(bitmap, new Rectangle(0, 0, browser.Width, browser.Height));
-                bitmap.Save("output.png", ImageFormat.Png);
+                bitmap.Save("../../../temp/output.png", ImageFormat.Png);
                 Console.WriteLine("HTML convert to image successfully!");
                 Application.Exit();
             }
